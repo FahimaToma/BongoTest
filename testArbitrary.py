bf3a5dd8704c36cf816cf383f7bc4f1af55482d2
FahimaToma <fahimaaktertoma@gmail.com>
2019-10-29 18:36:13 +0600

    2nd commit

--- testArbitrary.py
+++ testArbitrary.py
@@ -1,6 +1,10 @@
+
 from selenium import webdriver
 from random import randint
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
 import time
 driver = webdriver.Chrome\
     (r"D:\pythonPractise\pythontutorial\venv\Scripts\driver file\chromedriver.exe")
@@ -8,21 +12,19 @@ driver = webdriver.Chrome\
 driver.maximize_window()
 baseUrl = "https://www.bongobd.com/"
 driver.get(baseUrl)   # Getting Url.
-time.sleep(5)
-
-contents = driver.find_elements_by_id('content-selectors') # find only contents
-print(contents)
-
+driver.implicitly_wait(10)
 
 try:
-   for content in contents:
-       links = driver.find_elements_by_tag_name('a') # look for a href links.
-       l = links[randint(0, len(links) - 1)] # looging for random content
-       print(l.get_attribute("href"))
-       print("This link found")
-       time.sleep(2)
-       l.click()   # click the content
-       print(l)
-       print("Content Clicked!!")
+ #links = driver.find_elements_by_class_name("center-block") # lists of all the elements.
+
+ links = driver.find_elements_by_xpath('//*[@id="sm-container"]/div[1]/div[2]/div/div/div/div/div/div/div[2]/div/div/h6/a')
+ l = links[randint(0, len(links) - 1)] #looking for random elements.
+
+ print(l)
+ print(l.get_attribute("href"))
+ l.click()
+ print("Clicked")
+
 except:
-   print("Content isn't clickable.")
+ print("Element isn't clickable.")
+
